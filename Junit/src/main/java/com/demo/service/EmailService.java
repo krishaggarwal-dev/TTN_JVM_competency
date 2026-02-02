c2bf644f6508eab8ab482a638162b4a7d35c5135
krishaggarwal-dev <krishaggarwal124@gmail.com>
2026-02-02 09:50:44 +0530

    Modified code logic for test cases to be passed

--- Junit/src/main/java/com/demo/service/EmailService.java
+++ Junit/src/main/java/com/demo/service/EmailService.java
@@ -23,6 +23,13 @@ public class EmailService {
     }
 
     boolean sendEmail(Order order, String cc) {
+        String emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        if (cc == null || !cc.matches(emailRegex)) {
+            throw new IllegalArgumentException("Invalid CC email");
+        }
+        if (order == null) {
+            throw new IllegalArgumentException("Order cannot be null.");
+        }
         order.setCustomerNotified(true);
         return true;
     }
